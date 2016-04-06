6d3eec03d7f14b015867fd807a78632293562ce9
Brent Martin <bmart35@gmail.com>
2016-04-06 16:14:41 -0500

    Adding new and create to tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,38 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user, only: [:new, :create, :destroy] #TODO: Add Edit and Update
+
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = "Successfully created a new task"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  #TODO: Add Edit and Update Actions
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:notice] = "Deleted Task"
+    redirect_to :back
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  validates :description, length: { maximum: 300, minimum: 2 }
 end
