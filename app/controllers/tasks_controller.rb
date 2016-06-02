faaae7e45e88eb3fe713e122fa01933123f03acb
Brent Martin <bmart35@gmail.com>
2016-06-01 23:14:10 -0500

    updated params to have other task attributes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:description, :position, :completed, :user_id, :id)
   end
 end
